924c85c78c5fb72e87030687be700f958ab7d90e
fliu <fliu@anl.gov>
2021-09-23 08:09:50 +0000

    io

--- modelseedpy_escher/io/json.py
+++ modelseedpy_escher/io/json.py
@@ -6,4 +6,16 @@ def load_json_map(filename):
     """
     """
     with open(filename, "r") as fh:
-        return EscherMap(json.loads(fh.read()))
+        return EscherMap(json.load(fh))
+
+
+def save_json_map(em, filename):
+    with open(filename, "w") as fh:
+        fh.write(json.dumps(to_json(em)))
+
+
+def to_json(em):
+    """
+    Wrapper for later
+    """
+    return em.escher_data
